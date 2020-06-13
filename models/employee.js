d44f96d0be940d92af1024b98fe66c3029231991
Shreya A <sujnareddy242@gmail.com>
2020-06-13 21:35:09 +0530

    basic frontend

--- models/employee.js
+++ models/employee.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
 var Employee = mongoose.model('Employee', {
-    name: { type: String },
-    position: { type: String },
-    office: { type: String },
-    salary: { type: Number }
+    name: { 
+    	type: String
+    	 },
+    position: { 
+    	type: String
+    	 },
+    office: { 
+    	type: String
+    	 },
+    salary: { 
+    	type: Number 
+    }
 });
 
 module.exports = { Employee };
